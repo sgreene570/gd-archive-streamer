3791863e0b82aea169c5ac8fb4c86b70304d5339
sgreene570 <sgreene570@gmail.com>
2019-04-11 22:35:22 -0400

    Finish POC using ffmpeg cli

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # gd-archive-streamer
 
+
 Retrieve a random Grateful Dead show/song from Archive.org and listen to it in your terminal!
+
+Requires python3.
+
+# Installation
+
+
+`pip3 install -r requirements.txt`
+
+`ffmpeg` needs to be installed on your system. Refer to ffmpeg installation documentation.
+
--- gdarchive.py
+++ gdarchive.py
@@ -1,35 +1,42 @@
-from internetarchive import *
-
 import random
 import io
-import vlc
+import os
 from contextlib import redirect_stdout
+from internetarchive import *
 
 # Constants
 SEARCH_STRING = "collection:(GratefulDead AND stream_only)"
 
 def main():
     s = get_archive_session()
+    # Search archive.org
     search = s.search_items(SEARCH_STRING)
 
     resultCount = len(search)
 
+    # Create random show index
     index = random.randint(0, resultCount)
 
+    # Find random show
+    # TODO optimize iterator index
     identifier = list(search)[index]["identifier"]
 
+    # Redirect the output of the "dry_run" download function
     f = io.StringIO()
     with redirect_stdout(f):
         download(identifier, glob_pattern="*mp3", dry_run=True, ignore_errors=True)
     out = f.getvalue().splitlines()
 
+    # Pick random track from show
     index = random.randint(0, len(out) - 1)
     trackUrl = out[index]
 
-    stream_file(trackUrl)
+    # Play track
+    play_file(trackUrl)
 
-def stream_file(trackUrl):
-    print(trackUrl)
+def play_file(trackUrl):
+    # For now, use ffplay in terminal
+    os.system("ffplay" + " " + trackUrl)
 
 def get_archive_session():
     s = get_session()
