6913ceb0f6e79348d90703606ea3412b7649f8c8
sgreene570 <sgreene570@gmail.com>
2019-04-11 20:56:52 -0400

    Get mp3 streamale url

--- gdarchive.py
+++ gdarchive.py
@@ -1,6 +1,9 @@
 from internetarchive import *
 
 import random
+import io
+import vlc
+from contextlib import redirect_stdout
 
 # Constants
 SEARCH_STRING = "collection:(GratefulDead AND stream_only)"
@@ -14,7 +17,19 @@ def main():
     index = random.randint(0, resultCount)
 
     identifier = list(search)[index]["identifier"]
-    download(identifier, glob_pattern="*mp3", dry_run=True, ignore_errors=True)
+
+    f = io.StringIO()
+    with redirect_stdout(f):
+        download(identifier, glob_pattern="*mp3", dry_run=True, ignore_errors=True)
+    out = f.getvalue().splitlines()
+
+    index = random.randint(0, len(out) - 1)
+    trackUrl = out[index]
+
+    stream_file(trackUrl)
+
+def stream_file(trackUrl):
+    print(trackUrl)
 
 def get_archive_session():
     s = get_session()
