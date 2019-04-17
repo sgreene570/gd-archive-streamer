5cad848efd83431bbf61a5a9468fcf9ca0b1d828
sgreene570 <sgreene570@gmail.com>
2019-04-16 23:10:07 -0400

    Add cli arguments

--- gdarchive.py
+++ gdarchive.py
@@ -1,16 +1,41 @@
 import random
 import io
 import os
+import argparse
 from contextlib import redirect_stdout
 from internetarchive import *
 
-# Constants
-SEARCH_STRING = "collection:(GratefulDead AND stream_only)"
-
 def main():
+    # Argparse code
+    parser = argparse.ArgumentParser(description="Archive.org streamer commands")
+    parser.add_argument("-q", "--query", required=False, type=str, help="Archive search query string")
+    parser.add_argument("-s", "--silent", action="store_true", help="Disable audio stream playback")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Display track URL info")
+    args = parser.parse_args()
+
+    # Set default search string
+    search_string = "collection:(GratefulDead AND stream_only)"
+
+    if args.query is not None:
+        # Modify global search var
+        search_string = args.query
+
+    if args.verbose:
+        print(search_string)
+
+    # Runtime code
     s = get_archive_session()
+    url = find_file(s, search_string)
+
+    if args.verbose:
+        print(url)
+
+    if not args.silent:
+        play_file(url)
+
+def find_file(session, search_string):
     # Search archive.org
-    search = s.search_items(SEARCH_STRING)
+    search = session.search_items(search_string)
 
     resultCount = len(search)
 
@@ -31,12 +56,11 @@ def main():
     index = random.randint(0, len(out) - 1)
     trackUrl = out[index]
 
-    # Play track
-    play_file(trackUrl)
+    return trackUrl
 
 def play_file(trackUrl):
     # For now, use ffplay in terminal
-    os.system("ffplay" + " " + trackUrl)
+    os.system("ffplay" + " " + trackUrl + " > /dev/null 2>&1")
 
 def get_archive_session():
     s = get_session()
